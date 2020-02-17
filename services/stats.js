26ef54e5cc78c1aa224e5ea899f2e13210d0f048
CedricG44 <cedric-garcia@sfr.fr>
2020-02-17 15:37:32 +0100

    feat: anomalies by arondissement

--- services/stats.js
+++ services/stats.js
@@ -1,22 +1,46 @@
-const config = require('config');
-const indexName = config.get('elasticsearch.index_name');
+const config = require("config");
+const indexName = config.get("elasticsearch.index_name");
 
+// Compter le nombre d'anomalies par arondissement
 exports.statsByArrondissement = (client, callback) => {
-    // TODO Compter le nombre d'anomalies par arondissement
-    callback([]);
-}
+  client
+    .search({
+      index: indexName,
+      body: {
+        size: 0,
+        aggs: {
+          arrondissements: {
+            terms: {
+              field: "arrondissement.keyword",
+              size: 20
+            }
+          }
+        }
+      }
+    })
+    .then(response =>
+      callback(
+        response.body.aggregations.arrondissements.buckets.map(a => {
+          return {
+            arrondissement: a.key,
+            count: a.doc_count
+          };
+        })
+      )
+    );
+};
 
 exports.statsByType = (client, callback) => {
-    // TODO Trouver le top 5 des types et sous types d'anomalies
-    callback([]);
-}
+  // TODO Trouver le top 5 des types et sous types d'anomalies
+  callback([]);
+};
 
 exports.statsByMonth = (client, callback) => {
-    // TODO Trouver le top 10 des mois avec le plus d'anomalies
-    callback([]);
-}
+  // TODO Trouver le top 10 des mois avec le plus d'anomalies
+  callback([]);
+};
 
 exports.statsPropreteByArrondissement = (client, callback) => {
-    // TODO Trouver le top 3 des arrondissements avec le plus d'anomalies concernant la propreté
-    callback([]);
-}
+  // TODO Trouver le top 3 des arrondissements avec le plus d'anomalies concernant la propreté
+  callback([]);
+};
