7d150599772aca1072a1f8fcd8dbd77b7ba60752
CedricG44 <cedric-garcia@sfr.fr>
2020-02-17 15:01:50 +0100

    feat: CSV import

--- import.js
+++ import.js
@@ -1,28 +1,104 @@
-const config = require('config');
-const csv = require('csv-parser');
-const fs = require('fs');
-const { Client } = require('@elastic/elasticsearch');
-const indexName = config.get('elasticsearch.index_name');
+const config = require("config");
+const csv = require("csv-parser");
+const fs = require("fs");
+const { Client } = require("@elastic/elasticsearch");
+const indexName = config.get("elasticsearch.index_name");
 
-async function run () {
-    // Create Elasticsearch client
-    const client = new Client({ node: config.get('elasticsearch.uri') });
+async function run() {
+  // Create Elasticsearch client
+  const client = new Client({ node: config.get("elasticsearch.uri") });
 
-    // TODO il y a peut être des choses à faire ici avant de commencer ... 
+  // Création de l'indice
+  client.indices.create({ index: indexName }, (err, resp) => {
+    if (err) console.trace(err.message);
+  });
 
-    // Read CSV file
-    fs.createReadStream('dataset/dans-ma-rue.csv')
-        .pipe(csv({
-            separator: ';'
-        }))
-        .on('data', (data) => {
-            // TODO ici on récupère les lignes du CSV ...
-            console.log(data);
-        })
-        .on('end', () => {
-            // TODO il y a peut être des choses à faire à la fin aussi ?
-            console.log('Terminated!');
-        });
+  // Read CSV file
+  const signalements = [];
+  fs.createReadStream("dataset/dans-ma-rue.csv")
+    .pipe(
+      csv({
+        separator: ";"
+      })
+    )
+    .on("data", data => {
+      const signalement = {
+        timestamp: data.DATEDECL,
+        object_id: data.OBJECTID,
+        annee_declaration: data["ANNEE DECLARATION"],
+        mois_declaration: data["MOIS DECLARATION"],
+        type: data.TYPE,
+        sous_type: data.SOUSTYPE,
+        code_postal: data.CODE_POSTAL,
+        ville: data.VILLE,
+        arrondissement: data.ARRONDISSEMENT,
+        prefixe: data.PREFIXE,
+        intervenant: data.INTERVENANT,
+        conseil_de_quartier: data["CONSEIL DE QUARTIER"],
+        location: data.geo_point_2d
+      };
+      signalements.push(signalement);
+    })
+    .on("end", () => {
+      sendBulkInsertQuery(client, signalements).then(() => {
+        console.log("Tous les signalements insérés");
+        client.close();
+      });
+    });
+}
+
+// Fonction utilitaire permettant de découper les données en chunks puis d'envoyer les requêtes
+async function sendBulkInsertQuery(client, signalements) {
+  return new Promise(async (resolve, reject) => {
+    while (signalements.length) {
+      const { body: bulkResponse } = await client.bulk(
+        createBulkInsertQuery(signalements.splice(0, 20000))
+      );
+      if (bulkResponse.errors) {
+        reject();
+      }
+      console.log(`${bulkResponse.items.length} signalements insérés`);
+      resolve();
+    }
+  });
+}
+
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+function createBulkInsertQuery(signalements) {
+  const body = signalements.reduce((acc, signalement) => {
+    const {
+      timestamp,
+      annee_declaration,
+      mois_declaration,
+      type,
+      sous_type,
+      code_postal,
+      ville,
+      arrondissement,
+      prefixe,
+      intervenant,
+      conseil_de_quartier,
+      location
+    } = signalement;
+    acc.push({ index: { _index: indexName, _type: "_doc", _id: signalement.object_id } });
+    acc.push({
+      timestamp,
+      annee_declaration,
+      mois_declaration,
+      type,
+      sous_type,
+      code_postal,
+      ville,
+      arrondissement,
+      prefixe,
+      intervenant,
+      conseil_de_quartier,
+      location
+    });
+    return acc;
+  }, []);
+
+  return { body };
 }
 
 run().catch(console.error);
