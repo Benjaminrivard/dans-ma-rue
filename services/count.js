60ea7e301e63d37b4ef474b1f4892a60183c124d
Benjamin Rivard <benjamin.rivard@imt-altantique.net>
2020-02-17 15:33:24 +0100

    ElasticSearch query between dates

--- services/count.js
+++ services/count.js
@@ -1,16 +1,32 @@
-const config = require('config');
-const indexName = config.get('elasticsearch.index_name');
+const config = require("config");
+const indexName = config.get("elasticsearch.index_name");
 
 exports.count = (client, from, to, callback) => {
-    // TODO Compter le nombre d'anomalies entre deux dates
-    callback({
-        count: 0
+  // TODO Compter le nombre d'anomalies entre deux dates
+  const count = client
+    .count({
+      index: indexName,
+      body: {
+        query: {
+          range: {
+            timestamp: {
+              gte: from,
+              lt: to
+            }
+          }
+        }
+      }
     })
-}
+    .then(resp =>
+      callback({
+        count: resp.body.count
+      })
+    );
+};
 
 exports.countAround = (client, lat, lon, radius, callback) => {
-    // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
-    callback({
-        count: 0
-    })
-}
+  // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
+  callback({
+    count: 0
+  });
+};
